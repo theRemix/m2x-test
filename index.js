fc0c94017a22693c043c730536b73e79a6cafaed
Jon Borgonia <jon@gomagames.com>
2016-03-11 20:33:51 -1000

    create and delete streams

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
+'use strict';
+
 // const M2X = require("m2x");
 const request = require("request");
 const API = "http://api-m2x.att.com/v2";
-
-// const m2x = new M2X(process.env.M2X_APIKEY);
+const STREAM_TYPE = {
+  NUMERIC : "numeric",
+  ALPHANUMERIC : "alphanumeric"
+};
 
 const device_id = "3b0eaa66a0d88ade22a6af2b23378b72";
 
-const stream_id = "mm_pi";
-
 // console.log(makeStreamsEndpoint(device_id, stream_id, 'devices','value'));
 
+var streams = [];
+
+function createStream() {
+  let new_stream_id = Math.floor(Math.random()*9999);
+
+  request.put({
+      url : makeStreamsEndpoint(device_id, new_stream_id, 'devices',''),
+      json : true,
+      headers : {
+        "X-M2X-KEY" : process.env.M2X_APIKEY
+      },
+      body : {
+        display_name : `task_${new_stream_id}`,
+        type : STREAM_TYPE.ALPHANUMERIC
+      }
+    }, function(error, response, body){
+      console.log('made stream', new_stream_id, error || new Date());
+      console.log(body);
+      if(!error) streams.push(new_stream_id);
+    }
+  );
+
+}
+
+function deleteStream() {
+  let completed_stream_idx = Math.floor(Math.random()*streams.length);
+  let completed_stream_id = streams[completed_stream_idx];
+
+  request.del({
+      url : makeStreamsEndpoint(device_id, completed_stream_id, 'devices',''),
+      json : true,
+      headers : {
+        "X-M2X-KEY" : process.env.M2X_APIKEY
+      }    }, function(error, response, body){
+      console.log('deleted stream', completed_stream_id, error || new Date());
+      console.log(body);
+      if(!error) streams.splice(completed_stream_idx,1);
+    }
+  );
+
+}
+
 function hit(){
+  let stream_id = streams[Math.floor(Math.random()*streams.length)];
   setTimeout(() =>{
 
     request.put({
@@ -34,11 +79,28 @@ function hit(){
   }, Math.floor((Math.random()*500)+100)*10);
 }
 
-hit();
+
+function modStreams(){
+  setTimeout(() =>{
+
+    if(streams.length > 2 && Math.floor(Math.random()*2)){
+      deleteStream();
+    } else {
+      createStream();
+    }
+
+    modStreams();
+  }, Math.floor((Math.random()*500)+500)*10);
+}
 
 function makeStreamsEndpoint(device_id, stream_id, endpoint, action){
   return `${API}/${endpoint}/${device_id}/streams/${stream_id}/${action}`;
 }
+
+createStream();
+createStream();
+hit();
+modStreams();
 
 // m2x.devices.view(device_id, function(response) {
 //     console.log(response.json);
